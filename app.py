60c6a948b4b1c36d9c3a8e50c6865ef7dff27c99
EngMutabazi <engmutabazi2020@gmail.com>
2023-03-22 13:19:25 +0200

    create forms

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from form import RegistrationForm, LoginForm
 app = Flask(__name__) # This is the name of the module
-
+app.config['SECRET_KEY']= '6150293ab0b283698350acd7307257a2'
 posts= [
     {
     'author': 'van rossum',
@@ -27,6 +27,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form= RegistrationForm()
+    if form.validate_on_submit():
+        flash (f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', 
+                           title= 'register', form=form)
+
+@app.route("/login")
+def login():
+    form= LoginForm()
+    return render_template('login.html', 
+                           title= 'login', form=form)
 
 #set flask app run in debug mode
 if __name__== '__main__':
